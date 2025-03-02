47aff44ac5717fabf0b119ff9d847eca35151af6
nik-kita <nik@kita.github>
2025-03-02 02:34:22 +0100

    minimal draft gql2http functionality

--- gql2http.ts
+++ gql2http.ts
@@ -1,41 +1,87 @@
-import { OpenAPIHono } from "@hono/zod-openapi";
+import { OpenAPIHono, z } from "@hono/zod-openapi";
+import { analyzeDocument, gql } from "graphql-request";
 import { HTTPException } from "hono/http-exception";
-import * as graphql_http from "graphql-http";
+import { JsonHelper } from "./common/json.helper.ts";
 
 export const gql2http = new OpenAPIHono();
 
 gql2http.openapi({
   method: "post",
   path: "/",
+  summary:
+    "Parse graphql string into valid json object that should be used for make POST request to your grq-server",
+  description: `
+  * One operation at a time (root query/mutation block)
+  * How pass variables? (if you need)
+    Wrap the variables object with special comments:
+      * \`"""variables\`  before
+      * \`variables"""\` after
+    Example:
+    \`\`\`
+    """variables
+    {
+      hello: 'world'
+    }
+    variables"""
+    \`\`\`
+    P.S.
+    > As you can see in example - it is possible to pass relaxed json like in javascript (without strict raw JSON rules)
+  `,
+  request: {
+    body: {
+      content: {
+        "text/plain": {
+          schema: z.string(),
+        },
+      },
+    },
+  },
   responses: {
     200: {
-      description: "",
+      description:
+        "JSON object with <query> and optional <variables> and <operationName> properties",
     },
   },
 }, async (c) => {
-  try {
-    const maybeParams = await graphql_http.parseRequestParams<
-      typeof c.req,
-      typeof c
-    >({
-      body: await c.req.json(),
-      context: c,
-      headers: c.req.header(),
-      method: c.req.method,
-      raw: c.req,
-      url: c.req.url,
-    });
+  const text = await c.req.text();
+  const jBody = parse_text(text);
 
-    if (!maybeParams) {
-      return c.json(
-        { error: "not a well-formatted GraphQL over HTTP request" },
-        400,
-      );
-    }
-
-    return c.json(maybeParams);
-    // deno-lint-ignore no-explicit-any
-  } catch (err: any) {
-    throw new HTTPException(400, err);
-  }
+  return c.json(jBody);
 });
+
+function parse_text(text: string) {
+  const needle = '"""variables';
+  const startCandidate = text.indexOf(needle);
+  const endCandidate = startCandidate && text.lastIndexOf('variables"""');
+  const candidate = endCandidate &&
+    text.slice(startCandidate + needle.length, endCandidate);
+  const variables = candidate
+    ? (() => {
+      try {
+        return JsonHelper.parse(candidate);
+      } catch {
+        throw new HTTPException(400, {
+          message: "Unable to parse <variables> object",
+        });
+      }
+    })()
+    : undefined;
+
+  const query = candidate
+    ? (() => {
+      const textArr = text.split("");
+      textArr.splice(
+        startCandidate,
+        candidate.length + needle.length * 2,
+      );
+      return textArr.join("");
+    })()
+    : text;
+  const doc = analyzeDocument(gql`${query}`);
+
+  return {
+    variables,
+    query: doc.expression,
+    operationName: doc.operationName,
+  };
+}
